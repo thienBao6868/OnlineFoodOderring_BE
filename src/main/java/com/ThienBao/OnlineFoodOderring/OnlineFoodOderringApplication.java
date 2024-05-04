8e0eceb7121a58bca5d41f0d6d643d4211eb3f8e
Thienbao6868 <thienbao3018@gmail.com>
2024-05-04 15:56:14 +0700

    Initial Project Online_Food_Odder, Config DataSource connect Database

---
+++ src/main/java/com/ThienBao/OnlineFoodOderring/OnlineFoodOderringApplication.java
@@ -0,0 +1,13 @@
+package com.ThienBao.OnlineFoodOderring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOderringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodOderringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ThienBao/OnlineFoodOderring/OnlineFoodOderringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ThienBao.OnlineFoodOderring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOderringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
